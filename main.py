67e403b300afa02b3f73d7f4d293b6c348243141
Dmitry <103138302+nuafirytiasewo@users.noreply.github.com>
2024-09-01 21:21:37 +0400

    redoing the main file

--- main.py
+++ main.py
@@ -1,41 +1,33 @@
-from fastapi import FastAPI, HTTPException, Request
-import hmac
 import hashlib
-import os
-from dotenv import load_dotenv
+from urllib.parse import unquote
 
-# Загружаем переменные окружения из .env файла
-load_dotenv()
+def validate(hash_str, init_data, token, c_str="WebAppData"):
+    """
+    Validates the data received from the Telegram web app, using the
+    method documented here: 
+    https://core.telegram.org/bots/webapps#validating-data-received-via-the-web-app
 
-app = FastAPI()
+    hash_str - the has string passed by the webapp
+    init_data - the query string passed by the webapp
+    token - Telegram bot's token
+    c_str - constant string (default = "WebAppData")
+    """
 
-# Токен вашего Telegram бота
-BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+    init_data = sorted([ chunk.split("=") 
+          for chunk in unquote(init_data).split("&") 
+            if chunk[:len("hash=")]!="hash="],
+        key=lambda x: x[0])
+    init_data = "\n".join([f"{rec[0]}={rec[1]}" for rec in init_data])
 
-@app.post("/auth/")
-async def authenticate(request: Request):
-    data = await request.json()
+    secret_key = hmac.new(c_str.encode(), token.encode(),
+        hashlib.sha256 ).digest()
+    data_check = hmac.new( secret_key, init_data.encode(),
+        hashlib.sha256)
 
-    user = data.get("user")
-    auth_date = data.get("auth_date")
-    received_hash = data.get("hash")
+    return data_check.hexdigest() == hash_str
 
-    # Проверка наличия необходимых данных
-    if not user or not auth_date or not received_hash:
-        raise HTTPException(status_code=400, detail="Missing required fields")
+hash_str = "///"
+init_data = "///"
+token = "///"
 
-    # Создание строки для проверки хэша
-    data_check_string = f"user={user}&auth_date={auth_date}"
-    
-    # Вычисление хэша
-    secret_key = hmac.new(BOT_TOKEN.encode(), data_check_string.encode(), hashlib.sha256).hexdigest()
-
-    # # Сравнение хэша, присланного Telegram, и нашего (закоментить при самом первом тесте)
-    # if not hmac.compare_digest(secret_key, received_hash):
-    #     raise HTTPException(status_code=403, detail="Authentication failed")
-
-    # Если аутентификация прошла успешно, выведем приветственное сообщение в консоль
-    first_name = user.get("first_name", "User")
-    print(f"Привет, {first_name}!")
-
-    return {"status": "Authenticated"}
+print(validate(hash_str, init_data, token))
