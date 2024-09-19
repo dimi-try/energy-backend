37e094de6a19d5a335a9e3c58379204658e891a0
Evgeny <jenkneo.lal@gmail.com>
2024-09-19 22:14:41 +0400

    super refactoring initialize...

---
+++ app/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from .api import router as api_router
+
+app = FastAPI()
+
+# Подключаем роуты
+app.include_router(api_router)
+
+# Пример главной страницы
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
