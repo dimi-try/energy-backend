e15ec52f611a2ee0dfc507055f55d7dc536f976f
Evgeny <jenkneo.lal@gmail.com>
2024-09-29 22:20:20 +0400

    use CORS

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
 from .api import router as api_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "https://handy-redbird-visually.ngrok-free.app",
+    "http://localhost",  # Добавьте другие, если необходимо
+    "http://127.0.0.1",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Можно использовать ["*"] для разрешения всех источников (не рекомендуется для продакшена)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Подключаем роуты
 app.include_router(api_router)
